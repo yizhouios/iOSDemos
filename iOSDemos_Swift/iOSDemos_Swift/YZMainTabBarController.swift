d544afb256a930e75a10e7a735571a4c0ef9d968
dcdlife <71340227+95cc@users.noreply.github.com>
2025-03-15 09:02:36 +0800

    feat: Config UITabBarController.

---
+++ iOSDemos_Swift/iOSDemos_Swift/YZMainTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  YZMainTabBarController.swift
+//  iOSDemos_Swift
+//
+//  Created by yizhou on 2025/3/15.
+//
+
+import ESTabBarController_swift
+
+class YZMainTabBarController: ESTabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let v1 = UIViewController()
+        let v2 = UIViewController()
+        let v3 = UIViewController()
+        let v4 = UIViewController()
+        let v5 = UIViewController()
+        
+        v1.tabBarItem = ESTabBarItem.init(title: "Home", image: UIImage(named: "home"), selectedImage: UIImage(named: "home_1"))
+        v2.tabBarItem = ESTabBarItem.init(title: "Find", image: UIImage(named: "find"), selectedImage: UIImage(named: "find_1"))
+        v3.tabBarItem = ESTabBarItem.init(title: "Photo", image: UIImage(named: "photo"), selectedImage: UIImage(named: "photo_1"))
+        v4.tabBarItem = ESTabBarItem.init(title: "Favor", image: UIImage(named: "favor"), selectedImage: UIImage(named: "favor_1"))
+        v5.tabBarItem = ESTabBarItem.init(title: "Me", image: UIImage(named: "me"), selectedImage: UIImage(named: "me_1"))
+        
+        viewControllers = [v1, v2, v3, v4, v5]
+    }
+}
