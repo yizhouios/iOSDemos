3d5dba8b5fdf21f15acd6d8ed3265618c905851d
dcdlife <71340227+95cc@users.noreply.github.com>
2025-03-27 08:36:40 +0800

    feat: 完善 面试题tab页数据

--- iOSDemos_Swift/iOSDemos_Swift/Businesses/InterviewModule/YZInterviewViewController.swift
+++ iOSDemos_Swift/iOSDemos_Swift/Businesses/InterviewModule/YZInterviewViewController.swift
@@ -11,7 +11,7 @@ import SwiftyJSON
 class YZInterviewViewController: YZBaseViewController {
     let datas: JSON = [
         [
-            "title": "iOS面试题",
+//            "title": "iOS面试题",
             "items": [
                 [
                     "title": "《招聘一个靠谱的 iOS》—参考答案（上）",
@@ -22,8 +22,14 @@ class YZInterviewViewController: YZBaseViewController {
                     "url": "https://github.com/ChenYilong/iOSInterviewQuestions/blob/master/01%E3%80%8A%E6%8B%9B%E8%81%98%E4%B8%80%E4%B8%AA%E9%9D%A0%E8%B0%B1%E7%9A%84iOS%E3%80%8B%E9%9D%A2%E8%AF%95%E9%A2%98%E5%8F%82%E8%80%83%E7%AD%94%E6%A1%88/%E3%80%8A%E6%8B%9B%E8%81%98%E4%B8%80%E4%B8%AA%E9%9D%A0%E8%B0%B1%E7%9A%84iOS%E3%80%8B%E9%9D%A2%E8%AF%95%E9%A2%98%E5%8F%82%E8%80%83%E7%AD%94%E6%A1%88%EF%BC%88%E4%B8%8B%EF%BC%89.md"
                 ],
                 [
-                    "title": "《招聘一个靠谱的 iOS》—参考答案（下）",
-                    "url": "https://www.baidu.com"
+                    "title": "Over 150 iOS interview questions",
+                    "subTitle": "https://www.hackingwithswift.com/interview-questions",
+                    "url": "https://www.hackingwithswift.com/interview-questions"
+                ],
+                [
+                    "title": "Navigating the iOS Interview",
+                    "subTitle": "https://github.com/kodecocodes/ios-interview",
+                    "url": "https://github.com/kodecocodes/ios-interview"
                 ]
             ]
         ]
@@ -50,6 +56,19 @@ class YZInterviewViewController: YZBaseViewController {
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "roadmap", style: .plain, target: self, action: #selector(goRoadmap))
+    }
+    
+    @objc func goRoadmap() {
+        showWebPage(url: "https://roadmap.sh/ios")
+    }
+    
+    func showWebPage(url: String) {
+        let webVc = YZWebViewController()
+        webVc.urlString = url
+        webVc.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(webVc, animated: true)
     }
 }
 
@@ -80,23 +99,21 @@ extension YZInterviewViewController: UITableViewDataSource, UITableViewDelegate 
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell") ?? UITableViewCell.init(style: .subtitle, reuseIdentifier: "UITableViewCell")
 
+        let dict: [String: Any] = datas[indexPath.section]["items"].arrayValue[indexPath.row].rawValue as! [String : Any]
         cell.textLabel?.textColor = UIColor.init(white: 0.0, alpha: 0.6)
         cell.textLabel?.font = UIFont.init(name: "ChalkboardSE-Bold", size: 14.0)
         cell.textLabel?.lineBreakMode = .byCharWrapping
-        cell.textLabel?.text = datas[indexPath.section]["items"].arrayValue[indexPath.row]["title"].string
         cell.textLabel?.numberOfLines = 2
+        
+        cell.textLabel?.text = dict["title"] as? String
+        cell.detailTextLabel?.text = dict["subTitle"] as? String
 
         return cell
     }
     
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        let url = datas[indexPath.section]["items"].arrayValue[indexPath.row]["url"].string
-        
-        let webVc = YZWebViewController()
-        webVc.urlString = url
-        webVc.hidesBottomBarWhenPushed = true
-        navigationController?.pushViewController(webVc, animated: true)
+        let url = datas[indexPath.section]["items"].arrayValue[indexPath.row]["url"].string ?? ""
+        showWebPage(url: url)
     }
 }
