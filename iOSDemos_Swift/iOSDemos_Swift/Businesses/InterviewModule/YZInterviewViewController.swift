cc943170faee5491a67871e02f373116e72b4676
yizhou <71340227+yizhou@users.noreply.github.com>
2025-07-02 08:51:11 +0800

    Add 2020-2021面试题集合 (KCiOSGrocery).

--- iOSDemos_Swift/iOSDemos_Swift/Businesses/InterviewModule/YZInterviewViewController.swift
+++ iOSDemos_Swift/iOSDemos_Swift/Businesses/InterviewModule/YZInterviewViewController.swift
@@ -22,6 +22,10 @@ class YZInterviewViewController: YZBaseTableViewController {
                     "url": "https://github.com/ChenYilong/iOSInterviewQuestions/blob/master/01%E3%80%8A%E6%8B%9B%E8%81%98%E4%B8%80%E4%B8%AA%E9%9D%A0%E8%B0%B1%E7%9A%84iOS%E3%80%8B%E9%9D%A2%E8%AF%95%E9%A2%98%E5%8F%82%E8%80%83%E7%AD%94%E6%A1%88/%E3%80%8A%E6%8B%9B%E8%81%98%E4%B8%80%E4%B8%AA%E9%9D%A0%E8%B0%B1%E7%9A%84iOS%E3%80%8B%E9%9D%A2%E8%AF%95%E9%A2%98%E5%8F%82%E8%80%83%E7%AD%94%E6%A1%88%EF%BC%88%E4%B8%8B%EF%BC%89.md"
                 ],
                 [
+                    "title": "2020-2021面试题集合 (KCiOSGrocery)",
+                    "url": "https://github.com/LGCooci/KCiOSGrocery/blob/master/%E9%9D%A2%E8%AF%95%E9%A2%98%E9%9B%86%E5%90%88/2020-2021%E5%B9%B4%E6%9C%80%E6%96%B0%E9%9D%A2%E8%AF%95%E9%A2%98%E6%94%B6%E9%9B%86.md"
+                ],
+                [
                     "title": "Over 150 iOS interview questions",
                     "subTitle": "https://www.hackingwithswift.com/interview-questions",
                     "url": "https://www.hackingwithswift.com/interview-questions"
